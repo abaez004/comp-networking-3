56088f680e7ddb858d45ac7dcc6007ad0b8435ff
Angel Baez <51308340+abaez004@users.noreply.github.com>
2021-10-02 19:52:32 -0400

    Add files via upload

--- solution.py
+++ solution.py
@@ -94,6 +94,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     if recv5[:3] != '221':
         #print('221 reply not received from server.')
         pass
+    clientSocket.close()
     # Fill in end
 
 
